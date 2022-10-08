926599c39bf62371e1b3c892b9bbf68e06a82c0d
Heide Sebastião <107708768+heidesebastiao95@users.noreply.github.com>
2022-10-08 17:18:25 +0100

    Restore svg style

--- resources/views/admin/layouts/includes/css-links.blade.php
+++ resources/views/admin/layouts/includes/css-links.blade.php
@@ -3,5 +3,5 @@
 <link rel="stylesheet" href="{{ asset('css/tabler-payments.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/tabler-vendors.min.css') }}">
 <link rel="stylesheet" href="{{ asset('css/demo.min.css') }}">
-<link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+<link rel="stylesheet" href="{{ asset('css/icons/all.min.css') }}">
 <link rel="stylesheet" href="{{ asset('libs/dropzone/dropzone.css') }}">
