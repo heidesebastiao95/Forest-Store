9bfd3c40f6d943ac29c331de445cc2543ba20427
Heide Sebastião <107708768+heidesebastiao95@users.noreply.github.com>
2022-10-29 21:00:39 +0100

    Update sidebar

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.sellers.index');
+    return view('admin.index');
 });
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'),'verified'])->group(function () {
